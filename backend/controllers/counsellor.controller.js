8c43a7366ed2259ca41ca762576d60b3b744dfa7
jagnyadatta <jagnyadattadalai99215@gmail.com>
2025-05-02 13:14:50 +0530

    fixed small bug

--- backend/controllers/counsellor.controller.js
+++ backend/controllers/counsellor.controller.js
@@ -195,7 +195,7 @@ export const login = async (req, res) => {
       // console.log(allUser);
       res.status(200).json({ message: "fetch successfully",allUser, success: true });
     } catch (error){
-      console.error(err);
+      console.error(error);
       res.status(500).json({ message: "Internal server problem!", success: false });
     }
   }
