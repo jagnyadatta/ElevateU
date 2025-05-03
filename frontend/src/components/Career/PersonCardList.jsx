129830e5ac175bdbd8395edda14822f2f232eae2
jagnyadatta <jagnyadattadalai99215@gmail.com>
2025-05-04 01:34:08 +0530

    fixed small bug

--- frontend/src/components/Career/PersonCardList.jsx
+++ frontend/src/components/Career/PersonCardList.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import ProfileCard from "../ui/ProfileCard";
-import ChatBox from "@/check/ChatBox";
 
 const PersonCardList = ({persons}) => {
   return (
@@ -8,8 +7,6 @@ const PersonCardList = ({persons}) => {
       {persons.map((person, index) => (
         <ProfileCard key={index} name={person.name} image={person.profileImage} college={person.collegeName} id={person.slug} />
       ))}
-
-      <ChatBox/>
     </div>
   );
 };
