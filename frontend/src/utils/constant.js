3f8b811f4feccee2595463ccd49c2600653e2a5c
jagnyadatta <jagnyadattadalai99215@gmail.com>
2025-05-04 11:32:05 +0530

    set up done for chat box

--- frontend/src/utils/constant.js
+++ frontend/src/utils/constant.js
@@ -1,6 +1,7 @@
-export const STUDENT_API_END_POINT="http://localhost:8080/elevateu/v1/student";
-export const COUNSELLOR_API_END_POINT="http://localhost:8080/elevateu/v1/counsellor";
-export const OTP_API_END_POINT="http://localhost:8080/elevateu/v1/otp";
-export const AI_API_END_POINT="http://localhost:8080/elevateu/v1/aipage";
-export const FIND_USER_API_END_POINT="http://localhost:8080/elevateu/v1/user";
-export const CHAT_API_END_POINT="http://localhost:8080/elevateu/v1/chat";
+export const BACKEND_API_END_POINT = "http://localhost:8080";
+export const STUDENT_API_END_POINT=`${BACKEND_API_END_POINT}/elevateu/v1/student`;
+export const COUNSELLOR_API_END_POINT=`${BACKEND_API_END_POINT}/elevateu/v1/counsellor`;
+export const OTP_API_END_POINT=`${BACKEND_API_END_POINT}/elevateu/v1/otp`;
+export const AI_API_END_POINT=`${BACKEND_API_END_POINT}/elevateu/v1/aipage`;
+export const FIND_USER_API_END_POINT=`${BACKEND_API_END_POINT}/elevateu/v1/user`;
+export const CHAT_API_END_POINT=`${BACKEND_API_END_POINT}/elevateu/v1/chat`;
