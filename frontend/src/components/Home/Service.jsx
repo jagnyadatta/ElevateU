8c2cd0c8c8cb1924f799acec6a86d618b62c4e47
chandra <chandrashekharaprasad@gmail.com>
2025-05-07 16:48:52 +0530

    makeing responsive with home and service page

--- frontend/src/components/Home/Service.jsx
+++ frontend/src/components/Home/Service.jsx
@@ -2,53 +2,55 @@ import React, { useEffect } from "react";
 import BackgroundImage from "../shared/BackgroundImage";
 import { Button } from "../ui/button";
 import { Link } from "react-router-dom";
-import AOS from 'aos';
-import 'aos/dist/aos.css';
-
-
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 const Service = () => {
   useEffect(() => {
     AOS.init({
-      duration: 700,              
+      duration: 700,
     });
   }, []);
-  
+
   return (
     <>
       {/* Wrapper with relative for positioning context */}
       <div id="services" className="relative h-[100vh] w-full">
-
         {/* Main content above the background */}
-        <div  className="relative z-10 h-full flex flex-col items-center justify-center ">
-          <div className="w-full h-[20%]" >
-            <h2 className="text-5xl ml-[12%] mt-[7%] text-[#3b66ff] font-bold">
-             <a href="">Services</a>
+        <div className="relative z-10 h-full flex flex-col items-center justify-center ">
+          <div className="w-full sm:h-[20%]">
+            <h2 className="text-5xl ml-[25%] sm:ml-[12%] pb-[20px] sm:mt-[7%] text-[#3b66ff] font-bold">
+              <a className="" href="">Services</a>
             </h2>
           </div>
 
-          <div className="h-[80%] w-full flex flex-col justify-center items-center gap-5">
-            
+          <div className="h-full w-full flex flex-col justify-center items-center gap-5">
             {/* Card Row 1 */}
-            <div className="flex gap-2 " >
-              <div className="w-[45vw] h-[30vh] bg-[#3b66ff] flex flex-col justify-center items-center rounded-[2vw]" data-aos="fade-right">
-                <div className="w-[90%] h-[80%] flex flex-col gap-2" >
+            <div className="justify-center items-center h-[70%] sm:h-auto sm:justify-start md:justify-start lg:justify-start flex gap-2 ">
+              <div
+                className="w-[80%] sm:w-[45vw] h-full sm:h-[30vh] bg-[#3b66ff] flex flex-col justify-center items-center rounded-[2vw]"
+                data-aos="fade-right"
+              >
+                <div className="w-[90%] h-[80%] flex flex-col gap-2">
                   <h2 className="text-white text-4xl">Career counsellor</h2>
                   <p className="text-white">
                     Lorem ipsum dolor sit amet consectetur adipisicing elit.
                     Veritatis nisi officia est, obcaecati neque exercitationem
-                    nostrum labore eos amet rerum nihil id quasi illo minus? Earum
-                    placeat aut beatae! Blanditiis.
+                    nostrum labore eos amet rerum nihil id quasi illo minus?
+                    Earum placeat aut beatae! Blanditiis.
                   </p>
                   <Link to="/counsellor/all">
-                    <Button className="rounded-3xl bg-white text-[#3b66ff] w-[20%] hover:drop-shadow-[1px_1px_20px_white] transition-all duration-300 cursor-pointer">
+                    <Button className="rounded-3xl bg-white text-[#3b66ff] sm:w-[20%] hover:drop-shadow-[1px_1px_20px_white] transition-all duration-300 cursor-pointer">
                       Continue
                     </Button>
                   </Link>
                 </div>
               </div>
 
-              <div className="w-[35vw] h-[30vh] bg-[#3b66ff] flex justify-center rounded-[2vw]" data-aos="fade-down"  >
+              <div
+                className="w-[35vw] h-[30vh] bg-[#3b66ff] hidden sm:flex  justify-center rounded-[2vw]"
+                data-aos="fade-down"
+              >
                 <img
                   src="/image/women.png"
                   alt="element1"
@@ -58,8 +60,11 @@ const Service = () => {
             </div>
 
             {/* Card Row 2 */}
-            <div className="flex gap-2">
-              <div className="w-[35vw] h-[30vh] bg-[#3b66ff] flex justify-center rounded-[2vw] " data-aos="fade-up">
+            <div className="justify-center sm:justify-start md:justify-start lg:justify-start flex gap-2  h-[70%] sm:h-auto px-4 overflow-x-hidden ">
+              <div
+                className="w-[35vw] h-[30vh] bg-[#3b66ff] hidden sm:flex  justify-center rounded-[2vw]  "
+                data-aos="fade-up"
+              >
                 <img
                   src="/image/hand_shake_img.png"
                   alt="element1"
@@ -67,22 +72,27 @@ const Service = () => {
                 />
               </div>
 
-              <div className="w-[45vw] h-[30vh] bg-[#3b66ff] flex flex-col justify-center items-center rounded-[2vw]" data-aos="fade-left">
+              <div
+                className="w-[80%] sm:w-[45vw] h-full sm:h-[30vh] bg-[#3b66ff] flex flex-col justify-center items-center rounded-[2vw]"
+                data-aos="fade-left"
+              >
                 <div className="w-[90%] h-[80%] flex flex-col gap-2">
                   <h2 className="text-white text-4xl">Admission counselor</h2>
                   <p className="text-white">
                     Lorem ipsum dolor sit amet consectetur adipisicing elit.
                     Veritatis nisi officia est, obcaecati neque exercitationem
-                    nostrum labore eos amet rerum nihil id quasi illo minus? Earum
-                    placeat aut beatae! Blanditiis.
+                    nostrum labore eos amet rerum nihil id quasi illo minus?
+                    Earum placeat aut beatae! Blanditiis.
                   </p>
-                  <Button className="rounded-3xl bg-white text-[#3b66ff] w-[20%] hover:drop-shadow-[1px_1px_20px_white] transition-all duration-300 cursor-pointer">
+                  <Link to="">
+                  <Button className="rounded-3xl bg-white text-[#3b66ff] sm:w-[20%] hover:drop-shadow-[1px_1px_20px_white] transition-all duration-300 cursor-pointer">
                     Continue
                   </Button>
+                  </Link>
+          
                 </div>
               </div>
             </div>
-
           </div>
         </div>
       </div>
--- frontend/src/components/Home/Title.jsx
+++ frontend/src/components/Home/Title.jsx
@@ -7,8 +7,8 @@ export default function Title(){
     return (
         <>
         <div id="home" className="h-[100vh] w-full flex flex-col items-center justify-center sm:gap-4  col-span-2 sm:col-span-1 md:col-span-1 lg:col-span-1">
-          <div className="w-full sm:h-full flex flex-col justify-center items-center sm:items-end">
-            <div className="w-[96%] h-auto flex flex-col gap-5 sm:gap-3 items-center sm:items-start">
+          <div className="w-full  h-[70%] sm:h[full] flex flex-col justify-center items-center sm:items-end">
+            <div className="w-[96%] h-[50%] sm:h-auto flex flex-col gap-5 sm:gap-3 items-center sm:items-start">
               <h1 className="text-black text-[34px] sm:text-6xl">
                 Welcome to{" "}
                 <span className="font-bold text-[#3b66ff]">ElevateU</span>
@@ -20,14 +20,14 @@ export default function Title(){
               {/* <p className="text-justify">
                 Lorem ipsum, dolor sit amet consectetur adipisicing elit.
               </p> */}
-              <div class="w-[300px] sm:w-auto text-[18px] gap-1  sm:text-4xl font-medium text-gray-900 flex flex-row justify-start items-start">
+              <div class="w-[300px] sm:w-auto text-[18px] gap-1  sm:text-4xl font-medium text-gray-900 flex flex-row justify-start items-start pb-10 sm:pb-0">
                 <span class="font-normal">We offers</span>
                 <TypingEffect />
               </div>
               <Button className="rounded-3xl bg-[#3b66ff]">Go With..</Button>
             </div>
           </div>
-          <div className="w-full h-[20%] flex items-center justify-center gap-3 z-3">
+          <div className="w-full h-[20%] flex sm:items-center justify-center gap-3 z-3">
             <FontAwesomeIcon
               icon={faFacebook}
               className="text-[#3b66ff] text-3xl"
