8e33b8641ccc138f8a79b66d6cd4541d41bd9f57
jagnyadatta <jagnyadattadalai99215@gmail.com>
2025-06-27 15:52:41 +0530

    Fixed clearCookies on logout handle

--- backend/controllers/findUser.controller.js
+++ backend/controllers/findUser.controller.js
@@ -2,79 +2,90 @@ import { counsellorPerson } from "../models/counsellor.model.js";
 import { Student } from "../models/student.model.js";
 
 export const findUser = async (req, res) => {
-    try {
-      const { _id } = req.body;
-      if (!_id) {
-        return res.status(400).json({
-          message: "Something is missing",
-          success: false,
-        });
-      }
-
-        const user1 = await counsellorPerson.findOne({ _id });
-        if (user1) {
-          return res.status(200).json({
-            message: "Found user is COUNSELLOR!",
-            user1,
-            success: true,
-          });
-        }
-
-        const user2 = await Student.findOne({ _id });
-        if (user2) {
-          return res.status(200).json({
-            message: "Found user is STUDENT!",
-            user2,
-            success: true,
-          });
-        }
-  
-      return res.status(404).json({
-        message: "Account Not Found!",
-        success: false,
-      });
-    } catch (error) {
-      console.log(error);
-      return res.status(500).json({
-        message: "Internal Server Error",
+  try {
+    const { _id } = req.body;
+    if (!_id) {
+      return res.status(400).json({
+        message: "Something is missing",
         success: false,
       });
     }
-  };
+
+    const user1 = await counsellorPerson.findOne({ _id });
+    if (user1) {
+      return res.status(200).json({
+        message: "Found user is COUNSELLOR!",
+        user1,
+        success: true,
+      });
+    }
+
+    const user2 = await Student.findOne({ _id });
+    if (user2) {
+      return res.status(200).json({
+        message: "Found user is STUDENT!",
+        user2,
+        success: true,
+      });
+    }
+
+    return res.status(404).json({
+      message: "Account Not Found!",
+      success: false,
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      message: "Internal Server Error",
+      success: false,
+    });
+  }
+};
 
 export const findIndivisual = async (req, res) => {
-    try {
-      const { id } = req.params;
-        const user = await counsellorPerson.findOne({ _id: id });
-        // console.log(user);
-        if (user) {
-          return res.status(200).json({
-            message: "Found user is COUNSELLOR!",
-            user,
-            success: true,
-          });
-        }
-  
-        return res.status(404).json({
-          message: "Account Not Found!",
-          success: false,
-        });
-    } catch (error) {
-      console.log(error);
-      return res.status(500).json({
-        message: "Internal Server Error",
-        success: false,
+  try {
+    const { id } = req.params;
+    const user = await counsellorPerson.findOne({ _id: id });
+    // console.log(user);
+    if (user) {
+      return res.status(200).json({
+        message: "Found user is COUNSELLOR!",
+        user,
+        success: true,
       });
     }
-  };
+
+    return res.status(404).json({
+      message: "Account Not Found!",
+      success: false,
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      message: "Internal Server Error",
+      success: false,
+    });
+  }
+};
 
 //For Logout
 export const logout = async (req, res) => {
   try {
-    return res.status(200).cookie("token", "", { maxAge: 0 }).json({
-      message: "Logged out Successfully.",
-      success: true,
-    });
+    // return res.status(200).cookie("token", "", { maxAge: 0 }).json({
+    //   message: "Logged out Successfully.",
+    //   success: true,
+    // });
+    return res
+      .clearCookie("token", {
+        httpOnly: true,
+        sameSite: "strict",
+        secure: process.env.NODE_ENV === "production",
+      })
+      .status(200)
+      .json({
+        message: "Logged out Successfully.",
+        success: true,
+      });
   } catch (error) {
     console.log(error);
   }
